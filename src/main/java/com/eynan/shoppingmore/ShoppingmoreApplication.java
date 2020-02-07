bc3c07e28e29fa78d38f6f30d6988c82e17c9cac
Drori, Eynan <eynan_drori@bmc.com>
2020-02-07 15:24:06 +0200

    Update ShoppingmoreApplication.java

--- src/main/java/com/eynan/shoppingmore/ShoppingmoreApplication.java
+++ src/main/java/com/eynan/shoppingmore/ShoppingmoreApplication.java
@@ -1,6 +1,6 @@
 package com.eynan.shoppingmore;
 
-import org.ocpsoft.rewrite.servlet.DispatcherType;
+
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,13 +10,15 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
 import java.util.EnumSet;
 
+@Configuration
+@ComponentScan(basePackages={"com.eynan.*"})
 @EnableAutoConfiguration
-@ComponentScan({"com.auth0.samples.bootfaces"})
-@SpringBootApplication
 public class ShoppingmoreApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
